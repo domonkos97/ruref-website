6555b53efa7a8ccc5cde38cc56d0e282fd5499aa
domi <szabo.domonkos97@gmail.com>
2022-11-26 15:42:06 +0100

    initialize project

---
+++ src/main/java/org/ruref/application/RurefApplication.java
@@ -0,0 +1,13 @@
+package org.ruref.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RurefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RurefApplication.class, args);
+	}
+
+}
