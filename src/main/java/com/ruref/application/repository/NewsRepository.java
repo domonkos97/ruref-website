3901f09e18d0bc38cdc9dc9155de3bcbeab1154d
domonkos97 <szabo.domonkos97@gmail.com>
2023-02-04 13:43:36 +0100

    implement basic CRUD operations for news in news service

--- src/main/java/com/ruref/application/repository/NewsRepository.java
+++ src/main/java/com/ruref/application/repository/NewsRepository.java
@@ -3,5 +3,5 @@ package com.ruref.application.repository;
 import com.ruref.application.model.NewsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NewRepository extends JpaRepository<NewsModel, Long> {
+public interface NewsRepository extends JpaRepository<NewsModel, Long> {
 }
--- src/main/java/com/ruref/application/service/NewsService.java
+++ src/main/java/com/ruref/application/service/NewsService.java
@@ -1,24 +1,38 @@
 package com.ruref.application.service;
 
 import com.ruref.application.model.NewsModel;
+import com.ruref.application.repository.NewsRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class NewsService {
+
+    private final NewsRepository repository;
+
     public List<NewsModel> getAllNews(){
-        throw new UnsupportedOperationException();
+        return repository.findAll();
     }
-    public NewsModel getNewsById(UUID id){
-        throw new UnsupportedOperationException();
+    public NewsModel getNewsById(Long id){
+        return repository.getReferenceById(id);
     }
     public void addNews(NewsModel news){
-        throw new UnsupportedOperationException();
+        repository.save(news);
     }
-    public void updateNews(UUID id, NewsModel news){
-        throw new UnsupportedOperationException();
+    public void updateNews(Long id, NewsModel news){
+        NewsModel newsToUpdate = repository.getReferenceById(id);
+        newsToUpdate.setAuthor(news.getAuthor());
+        newsToUpdate.setBody(news.getBody());
+        newsToUpdate.setCategory(news.getCategory());
+        newsToUpdate.setTitle(news.getTitle());
+        newsToUpdate.setDateOfArticle(news.getDateOfArticle());
+        repository.save(newsToUpdate);
     }
-    public void deleteNews(UUID id){
-        throw new UnsupportedOperationException();
+    public void deleteNews(Long id){
+        repository.delete(repository.getReferenceById(id));
     }
 }
