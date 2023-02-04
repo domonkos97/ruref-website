82e1c9408feed2843c8e1b22d56658bf1db9a464
domonkos97 <szabo.domonkos97@gmail.com>
2023-02-04 11:50:19 +0100

    add lombok annotations to userModel

--- src/main/java/com/ruref/application/model/UserModel.java
+++ src/main/java/com/ruref/application/model/UserModel.java
@@ -2,8 +2,16 @@ package com.ruref.application.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserModel {
     @Id
     private long id;
