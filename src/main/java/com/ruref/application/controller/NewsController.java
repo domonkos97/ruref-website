1c82f466ac8670b045ffd4533890c5d22fe32f32
domonkos97 <szabo.domonkos97@gmail.com>
2023-02-04 14:38:46 +0100

    implement add news endpoint

--- src/main/java/com/ruref/application/controller/NewsController.java
+++ src/main/java/com/ruref/application/controller/NewsController.java
@@ -3,10 +3,9 @@ package com.ruref.application.controller;
 import com.ruref.application.model.NewsModel;
 import com.ruref.application.service.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,11 @@ public class NewsController {
     @GetMapping("/{id}")
     public NewsModel getNewsById(@PathVariable Long id){
         return service.getNewsById(id);
+    }
+
+    @PostMapping("/add")
+    public void addNews(@RequestBody NewsModel news) {
+        service.addNews(news);
     }
 
 }
