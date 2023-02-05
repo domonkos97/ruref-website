8b9cc301879ff9ae5a07915732335a6b1240bba8
domonkos97 <szabo.domonkos97@gmail.com>
2023-02-05 16:44:27 +0100

    create news DTO

--- src/main/java/com/ruref/application/model/dto/NewsDTO.java
+++ src/main/java/com/ruref/application/model/dto/NewsDTO.java
@@ -1,27 +1,24 @@
-package com.ruref.application.model;
+package com.ruref.application.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import com.ruref.application.model.UserModel;
+import jakarta.persistence.ElementCollection;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
-@Entity
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class NewsModel {
-    @Id
-    @GeneratedValue
+@Data
+public class NewsDTO {
     private Long id;
     private LocalDate dateOfArticle;
-    @Transient
     private UserModel author;
     private String title;
     private String body;
     @ElementCollection
     private List<String> category;
+
 }
