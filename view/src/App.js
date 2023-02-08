00774f07095642310dc769acad34d6752f42d068
domonkos97 <szabo.domonkos97@gmail.com>
2023-02-08 10:41:08 +0100

    implement fetch news

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Home from "./routes/Home";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <Home></Home>
-  );
+    const [news, setNews] = useState(null)
+
+    const getApi = async (url) => {
+        let response = await fetch(url)
+        let data = response.json()
+        setNews(data)
+    }
+    useEffect(() => {
+        getApi("/route/to/api").catch(console.error)
+    }, [])
+
+
+
+    return (
+    <Home news={news}></Home>
+    );
 }
 
 export default App;
