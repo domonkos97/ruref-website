b6ffd97cdbedfc961cfba7ec81f570b5f3571e69
Domonkos Szabo <domi@domi.ubuntu>
2023-03-25 10:34:21 +0100

    fix news layout

--- view/src/components/NewsArticle.js
+++ view/src/components/NewsArticle.js
@@ -1,8 +1,11 @@
-function NewsArticle( {article} ) {
+function NewsArticle( {article, data} ) {
     return (
         <div className="picture-container">
-            <img className="picture" src={article.photo} alt="photo"></img>
-            <div className="title-news">{article.title}</div>
+            <img className={`picture ${data}`} src={article.photo} alt="photo"></img>
+            <div className="title-body-news flex justify-between">
+                <div className="title-news">{article.title}</div>
+                <div className="body-news">{article.body}</div>
+            </div>
         </div>
     );
 
--- view/src/components/NewsSection.js
+++ view/src/components/NewsSection.js
@@ -4,6 +4,7 @@ import {useEffect, useRef, useState} from "react";
 
 function NewsSection({ news }) {
     const myRef = useRef();
+    const picturesRef = useRef();
     const [myElementIsVisible, setMyElementIsVisible] = useState(false);
     let options = {
         threshold: 0.3,
@@ -23,32 +24,35 @@ function NewsSection({ news }) {
         }, options);
         observer.observe(myRef.current);
 
-        // cleanup observer on component unmount
         return () => {
             observer.disconnect();
         };
     }, []);
 
+
+
     return (
         <div
-            className="news-section flex flex-col items-center justify-center h-screen p-28 z-0"
+            className="news-section flex flex-col items-center z-0 justify-center h-screen p-28 z-0"
             ref={myRef}
         >
             <div className={`content ${myElementIsVisible ? "show" : "hidden"}`} >
-                <div className="pictures flex justify-between gap-2 mb-2">
+                <div className="pictures flex gap-2 mb-2">
                     {news.slice(0, 1).map((article) => (
-                        <NewsArticle article={article}></NewsArticle>
+                        <NewsArticle article={article} data="one"></NewsArticle>
                     ))}
                     {news.slice(1, 2).map((article) => (
-                        <NewsArticle article={article}></NewsArticle>
+                        <NewsArticle article={article} data="two"></NewsArticle>
                     ))}
+
                 </div>
-                <div className="pictures flex justify-between gap-2">
-                    {news.slice(3, 4).map((article) => (
-                        <NewsArticle article={article}></NewsArticle>
-                    ))}
+
+                <div className="pictures flex justify-between h-contain w-contain gap-2 mb-2" ref={picturesRef}>
                     {news.slice(2, 3).map((article) => (
-                        <NewsArticle article={article}></NewsArticle>
+                        <NewsArticle article={article} data="three"></NewsArticle>
+                    ))}
+                    {news.slice(3, 4).map((article) => (
+                        <NewsArticle article={article} data="four"></NewsArticle>
                     ))}
                 </div>
             </div>
