546c16d61935adb83143a4eee3c26050d095a9ec
Domonkos Szabo <domi@domi.ubuntu>
2023-03-27 13:57:43 +0200

    add new font family and change hero styling

--- view/src/components/TitleCard.js
+++ view/src/components/TitleCard.js
@@ -1,8 +1,18 @@
 function TitleCards( {isVisible} ) {
     return (
-        <div className={`title flex flex-col justify-center items-end z-0 ${isVisible ? 'show': 'hidden'}`}>
-            <p className="text-3xl font-sans red-text-color">Rákospalota Újvárosi Református Gyülekezet</p>
-            {/*<p className="big-text font-black red-text-color">RUREF</p>*/}
+        <div className={`title flex flex-col justify-between items-center z-0 ${isVisible ? 'show': 'hidden'}`}>
+            <div className="relative">
+                <p className="big-text syne font-black double-text-color text-center tracking-wide w-1/2 ">ruref</p>
+
+            </div>
+            <div className="relative">
+                <div className="line secondary-bg-color w-full h-2 absolute -top-4"></div>
+                <div className="text w-6/7">
+                    <p className="text-3xl font-sans grey-text-color  text-center tracking-widest">Rákospalota Újvárosi </p>
+                    <p className="text-3xl font-bold font-sans grey-text-color  text-center tracking-wide">Református Gyülekezet </p>
+                </div>
+                <div className="line secondary-bg-color w-full h-2 absolute -bottom-4"></div>
+            </div>
         </div>
     );
 }
